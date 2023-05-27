23c16e0f44c705032efd5f6de52bd743f365f6cf
roma <noemail@void.com>
2023-05-27 18:00:01 +0300

    init

---
+++ RxJsTest/wwwroot/index.js
@@ -0,0 +1,27 @@
+﻿//import { fromEvent } from 'rxjs';
+let firstStream$ =
+    rxjs.fromEvent(document.getElementById('first'), 'input')
+        .pipe(
+            rxjs.map(ev => ev.target.value),
+            rxjs.filter(val => val.length > 3) // здесь можно вылидировать
+        );
+
+let secondStream$ =
+    rxjs.fromEvent(document.getElementById('second'), 'input')
+            .pipe(
+                rxjs.map(ev => ev.target.value),
+                rxjs.filter(val => val.length > 3) // здесь можно вылидировать
+            );
+
+rxjs.combineLatest(firstStream$, secondStream$)
+    .pipe(
+        rxjs.map(([first, second]) => {
+            return {
+                first: first,
+                second: second
+            };
+        })
+    )
+    .subscribe((ev) => {
+        console.log(ev);
+    });
